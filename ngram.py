330b84298e1a94a74b52e567d981ff21b694eb20
coffee247 <B$C$dreamer2020>
2020-02-09 15:42:28 -0500

    Good progress

--- ngram.py
+++ ngram.py
@@ -12,6 +12,8 @@ TODO try this REGEX --> ([\S]+?[[\S\s]+?(?:[\.?!]))
 import re
 import sys
 import logging
+import random
+from _collections import defaultdict
 
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
@@ -29,33 +31,124 @@ def readfile(fileAtIndex):
         print("Bye!\n\n")
         exit()
 
+def makeUnigramTable(str, counts):
+    words = str.split()
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+
+def getwordCount(str):
+    count = 0
+    words = str.split()
+    for word in words:
+            count += 1
+    return count - 1
+
+def cleanText(filetext):
+    filetext = filetext.lstrip()
+    filetext = filetext.lower()
+    filetext = re.sub(r'[\[\]\(\):,_]', '', filetext)  # remove colon, comma, [ and ] characters,
+    filetext = re.sub(r'[\"\”\“\*\’\‘]', '', filetext)  # remove quote marks and *
+    filetext = re.sub(r'(\n)', ' ', filetext)  # turn newlines into spaces
+    filetext = re.sub(r'(\s{2,})', ' ', filetext)  # remove multiple whitespace characters
+    filetext = re.sub(r'(\.{2,})', ' ', filetext)  # remove multiple periods
+    filetext = re.sub(r'(\-{2,})', ' ', filetext)  # remove multiple hyphens
+    filetext = re.sub(r'(\—)', ' ', filetext)  # replace long dash with space
+    filetext = re.sub(r'(\s{2,})', ' ', filetext)  # remove multiple whitespace characters
+    filetext = re.sub(r'[!] ', ' !\n', filetext)  # add newline after exclamation
+    filetext = re.sub(r'[.] ', ' .\n', filetext)  # add newline after period
+    filetext = re.sub(r'[?] ', ' ?\n', filetext)  # add newline after question mark
+    return filetext
+
+def makeCodedSentences(filetext, n):
+    n = n-1
+    start = '<start> '
+    end = ' <end>'
+    newList = []
+    for line in filetext:
+        sentence = str(line)
+        if (getwordCount(sentence) >= n):
+            sentence = "{}{}{}".format(n*start, sentence, end)
+            newList.append(sentence)
+    return newList
+
+def makeNgramTable(filetext, n, lookupTable):
+
+    for line in filetext:
+        words = line.split(' ')
+        for j in range(len(words)-n):
+            ngram = ""
+            k = j + n-1
+            for l in range(j, k, 1):
+                ngram = (ngram + ' ' + words[l]).lstrip()
+                if (len(ngram.split(' ')) == (n-1)):
+                    try:
+                        count = lookupTable[ngram][words[l+1]]
+                        lookupTable[ngram][words[l + 1]] = count + 1
+                    except:
+                        count = 0
+            lookupTable[ngram][words[l + 1]] = count + 1
+
+def makeRelFreqTable(unigramTable, nGramTable, relFreqTable):
+    for ngramKey in nGramTable:
+        unigramKey = nGramTable[ngramKey]
+        for val in list(unigramKey):
+            if val is not '':
+                numerator = nGramTable[ngramKey][val]
+                denominator = unigramTable[val]
+                relFreqTable[ngramKey][val] = round(numerator/denominator, 4)
+
+
+
+def getUnigramRawFreq(unigram_table, instr):
+    returnString = unigram_table[instr]
+    return int(returnString)
+
+def getNgramRawFreq(nGramTable, instr):
+    returnString = nGramTable[instr]
+    return int(returnString)
 
 
 def main():
     filetext = ""
-    n = sys.argv[1]
-    m = sys.argv[2]
+    n = int(sys.argv[1])
+    m = int(sys.argv[2])
+    firstwords = []
+    lookupTable = defaultdict(dict)
+    counts = dict()
+    relFreqTable = defaultdict(dict)
+
 
     print("\nThis program generates random sentences based on an Ngram model."
           "\nAuthored by: James M. Stallings\nVCU student ID: V00859712\n\n")
     print("Command line settings:  {} {} {}".format(sys.argv[0], n, m))
 
-    for i in range(3, len(sys.argv), 1):
-        filetext = "{} {}".format(filetext, readfile(i))
-    filetext = filetext.lstrip()
-    filetext = re.sub(r'[\[\]_]','',filetext.rstrip())  # remove [ and ] characters
-    filetext = re.sub(r'(\n)',' ',filetext)  # turn newlines into spaces
-    filetext = re.sub(r'(\")','',filetext)  # remove double quotes
-    filetext = re.sub(r'(\.{2,})', ' ', filetext)  # remove multiple periods
-    filetext = re.sub(r'(\-{2,})', ' ', filetext)  # remove multiple hyphens
-    filetext = re.sub(r'(\s{2,})', ' ', filetext)  # remove multiple whitespace characters
-    filetext = re.sub(r'[!] ', '!\n', filetext)  # add newline after exclamation
-    filetext = re.sub(r'[.] ', '.\n', filetext)  # add newline after period
-    filetext = re.sub(r'[?] ', '?\n', filetext)  # add newline after question mark
-    filetext = filetext.split('\n')  # create new list from filetext split on newline.
-    for line in filetext:
-        print(line)
+    for i in range(3, len(sys.argv), 1):  # for each input file ...
 
+        '''read the file into memory as a string object & append to filetext '''
+        filetext = "{} {}".format(filetext, readfile(i))
+
+    ''' prepare the text for processing '''
+    filetext = cleanText(filetext)
+
+
+
+    filetext = filetext.split('\n')  # create new list from filetext split on newline.
+    filetext = makeCodedSentences(filetext, int(n))
+    for line in filetext:
+        ''' produce a Unigram table of raw frequencies '''
+        unigram_table = makeUnigramTable(line, counts)
+
+
+
+
+    makeNgramTable(filetext, n, lookupTable)
+    makeRelFreqTable(unigram_table, lookupTable, relFreqTable)
+    print(relFreqTable['named the empress']['anna'])
+    print(relFreqTable['falling into angry']['discussions'])
 
 if __name__ == '__main__':
     main()
